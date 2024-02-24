dd3665ffd7f6fe740f842e5158ae05fe46a7ee64
PekiDroiid <luiscp_51@hotmail.com>
2024-02-24 11:07:09 -0500

    Lista de datos

---
+++ src/components/Menu/MenuItem.vue
@@ -0,0 +1,11 @@
+<template>
+<div>
+    <<router-link :to="data.path">{{ data.name }}</router-link>
+</div>
+
+</template>
+<script lang="ts" setup>
+import type IMenuItem from '@/interface/IMenuItem'
+const data = defineProps<IMenuItem>();
+
+</script>
---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <MenuItem v-for = "(routes, index) in routeViews" :name="routes.name" :path="routes.path"::key="index"></MenuItem>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import type IMenuItem from '@/interface/IMenuItem'
+const data = defineProps<IMenuItem>();
+
+</script>
